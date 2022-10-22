c4388b5327173374873fd5e6973f3f6e25f9fb9f
Vladyslav <zaichenkovladyslav@gmail.com>
2022-10-22 12:56:34 -0400

    final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@
  *
  * @version 22.06.2020
  * @author Vladyslav Zaichenko
-
+ *
  * Module 4 task 1
  *
  * Home task. Files management.
@@ -43,85 +43,66 @@ public class Main {
 
         /*
          * @param startSpring return time start
-         * counting ERROR lines in the String.split way
          */
         LocalDateTime startSpring = LocalDateTime.now();
 
         /*
         * @param text return text from file logs.txt
         */
-        String text = new String(Files.readAllBytes(Paths.get
-                ("/Users/Vladyslav/Desktop/logs.txt")));
+        String text = new String(Files.readAllBytes(Paths.get("/Users/Vladyslav/Desktop/logs.txt")));
 
         /*
-         * @param lines return text return
-         * String array of lines from file logs.txt
+         * @param lines return string array of lines from file logs.txt
          */
         String[] lines = text.split(System.lineSeparator());
 
-        System.out.println("\n This file logs.txt contains: " +
-                lines.length + " lines");
+        System.out.println("\n This file logs.txt contains: " + lines.length + " lines");
 
         /*
-         * @param counterErrors return count of lines
-         * with ERROR from file logs.txt
+         * @param counterErrors return count of lines with ERROR from file logs.txt
          */
         int counterErrors = 0;
+
         for (int i = 0; i < lines.length; i++) {
-            if (lines[i].contains("ERROR")){
+            if (lines[i].contains("ERROR")) {
                 counterErrors++;
             }
         }
 
-        System.out.println
-                ("\n This file logs.txt contains: "
-                        + counterErrors + " ERROR logs");
+        System.out.println("\n This file logs.txt contains: " + counterErrors + " ERROR logs");
 
         /*
          * @param finishSpring return time finish
-         * counting ERROR lines in the String.split way
          */
         LocalDateTime finishSpring = LocalDateTime.now();
 
         System.out.println("Using String.split for finding ERRORS needs "
-                + ChronoUnit.MILLIS.between(startSpring, finishSpring)
-                + " milliseconds");
-
-
+                + ChronoUnit.MILLIS.between(startSpring, finishSpring) + " milliseconds");
 
         System.out.println
                 ("\n ------------------- PART 2 -------------------");
 
-
         /*
          * @param startFiles return time start
-         * counting ERROR lines in the String.split way
          */
         LocalDateTime startFiles = LocalDateTime.now();
 
         /*
-         * @param counterErrors2 return count of lines
-         * with ERROR from file logs.txt
+         * @param counterErrors2 return count of lines with ERROR from file logs.txt
          */
-        final long counterErrors2 =
-                Files.lines(Paths.get("/Users/Vladyslav/Desktop/logs.txt"))
+        final long counterErrors2 = Files.lines(Paths.get("/Users/Vladyslav/Desktop/logs.txt"))
                 .filter(line -> line.contains("ERROR"))
                 .count();
 
-        System.out.println("\n This file logs.txt contains: "
-                + counterErrors2 + " ERROR logs");
+        System.out.println("\n This file logs.txt contains: " + counterErrors2 + " ERROR logs");
 
         /*
          * @param finishFiles return time finish
-         * counting ERROR lines in the String.split way
          */
         LocalDateTime finishFiles = LocalDateTime.now();
 
         System.out.println("Using Files.lines for finding ERRORS needs "
-                + ChronoUnit.MILLIS.between(startFiles, finishFiles)
-                + " milliseconds");
-
-
+                + ChronoUnit.MILLIS.between(startFiles, finishFiles) + " milliseconds");
 
         System.out.println
                 ("\n ------------------- PART 3 -------------------");
@@ -131,8 +112,7 @@ public class Main {
          * with date October 2019 from file logs.txt
          * (from stream to list)
          */
-        List<String> theOctober2019Logs =
-                Files.lines(Paths.get("/Users/Vladyslav/Desktop/logs.txt"))
+        List<String> theOctober2019Logs = Files.lines(Paths.get("/Users/Vladyslav/Desktop/logs.txt"))
                 .filter(line -> line.contains("2019-10-"))
                         .collect(Collectors.toList());
 
@@ -144,13 +124,16 @@ public class Main {
 
         // Creation file October2019logs.txt and recording data from octoberLogs
         Path path = Paths.get("/Users/Vladyslav/Desktop/October2019logs.txt");
+
         Files.write(path, octoberLogs.getBytes());
 
         System.out.println
-                ("\n File with all logs from October 2019 on your Desktop");
+                ("\nFile with all logs from October 2019 on your Desktop");
 
         // ------------ MY OUTPUT -------------
+
         /*
+
         ------------------- PART 1 -------------------
 
         This file logs.txt contains: 2845607 lines
@@ -166,6 +149,7 @@ public class Main {
         ------------------- PART 3 -------------------
 
         File with all logs from October 2019 on your Desktop
+
         */
     }
 }
